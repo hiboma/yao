cc48401b769730bd14f6384c1ac4bba808cdeff8
hiboma <hito@pepabo.com>
2019-08-14 18:22:04 +0900

    add tests Yao::SecurityGroupRule#port, #port_range, #remote_ip_cidr

--- test/yao/resources/test_security_group_rule.rb
+++ test/yao/resources/test_security_group_rule.rb
@@ -20,4 +20,44 @@ class TestSecurityGroup < Test::Unit::TestCase
     assert_equal(rule.port_range_min, "443")
     assert_equal(rule.ethertype, "IPv4")
   end
+
+  sub_test_case 'port_range_max == port_range_min' do
+
+    def test_port_and_port_range
+
+      params = {
+        "port_range_max"    => "443",
+        "port_range_min"    => "443",
+      }
+
+      rule = Yao::SecurityGroupRule.new(params)
+      assert_equal(rule.port, "443")
+      assert_equal(rule.port_range, "443".."443")
+    end
+  end
+
+  sub_test_case 'port_range_max > port_range_min' do
+
+    def test_port_and_port_range
+
+      params = {
+        "port_range_max"    => "1024",
+        "port_range_min"    => "512",
+      }
+
+      rule = Yao::SecurityGroupRule.new(params)
+      assert_equal(rule.port, "1024".."512")
+      assert_equal(rule.port_range, "1024".."512")
+    end
+  end
+
+  def test_remote_ip_cidr
+
+    params = {
+      "remote_ip_prefix"  => "10.0.0.0/24",
+    }
+
+    rule = Yao::SecurityGroupRule.new(params)
+    assert_equal(rule.remote_ip_cidr, "10.0.0.0/24")
+  end
 end
